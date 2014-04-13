56bbfcd0b36f0178318a9320d10ae109e61c6a37
mzazzali <mike@zee03.com>
2014-04-13 08:25:17 -0400

    created functions to convert single and tens digits

--- src/zee3/alphaconverter.clj
+++ src/zee3/alphaconverter.clj
@@ -1,5 +1,8 @@
 (ns zee3.alphaconverter)
 
+(def digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
+(def tens [nil "ten" "twenty" "thirty" "forty" "fifthy" "sixty" "seventy" "eighty" "ninety"])
+
 (defn get-decimal
   [decimal]
   (mod (int (* decimal 100)) 100))
@@ -10,6 +13,12 @@
   (let [decimal (get-decimal number)])
     (str (get-decimal number) "/100"))
 
+(defn convert-single-digit
+  "convert a digit to a text representation"
+  [d]
+  (nth digits d))
 
-
-
+(defn convert-tens-digit
+  "convert the tens digit to a text representation"
+  [d]
+  (nth tens d))
--- test/zee3/alphaconverter_test.clj
+++ test/zee3/alphaconverter_test.clj
@@ -13,3 +13,27 @@
     (is (= 0 (get-decimal 1000)))
     (is (= 0 (get-decimal 1000.0)))
     (is (= 90 (get-decimal 1000.9)))))
+
+(deftest test-convert-single-digit
+  (testing "converting a single digit"
+          (is (= "one" (convert-single-digit 1)))
+          (is (= "two" (convert-single-digit 2)))
+          (is (= "three" (convert-single-digit 3)))
+          (is (= "four" (convert-single-digit 4)))
+          (is (= "five" (convert-single-digit 5)))
+          (is (= "six" (convert-single-digit 6)))
+          (is (= "seven" (convert-single-digit 7)))
+          (is (= "eight" (convert-single-digit 8)))
+          (is (= "nine" (convert-single-digit 9)))
+          (is (= "zero" (convert-single-digit 0)))))
+
+(deftest test-convert-tens-digit
+  (testing "converting the tends digit"
+          (is (= "twenty" (convert-tens-digit 2)))
+          (is (= "thirty" (convert-tens-digit 3)))
+          (is (= "forty" (convert-tens-digit 4)))
+          (is (= "fifthy" (convert-tens-digit 5)))
+          (is (= "sixty" (convert-tens-digit 6)))
+          (is (= "seventy" (convert-tens-digit 7)))
+          (is (= "eighty" (convert-tens-digit 8)))
+          (is (= "ninety" (convert-tens-digit 9)))))
