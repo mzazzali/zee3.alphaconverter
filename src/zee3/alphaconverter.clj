5d78c2e60429ee91ddd9e843f58947aa09a0ff97
mzazzali <mike@zee03.com>
2014-04-13 23:58:00 -0400

    processing hundreds place now

--- src/zee3/alphaconverter.clj
+++ src/zee3/alphaconverter.clj
@@ -1,9 +1,14 @@
 (ns zee3.alphaconverter)
 
 (def digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
-(def tens [nil "ten" "twenty" "thirty" "forty" "fifthy" "sixty" "seventy" "eighty" "ninety"])
+(def tens [nil "ten" "twenty" "thirty" "forty" "fifty" "sixty" "seventy" "eighty" "ninety"])
 (def teens ["ten" "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen" "eighteen" "nineteen"])
 (def tens-digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen" "eighteen" "nineteen"])
+
+(defn get-place-descriptor
+  [m]
+  (cond
+   (<= m 1000) "hundred"))
 
 (defn get-decimal
   [decimal]
@@ -30,8 +35,19 @@
   (nth tens-digits d))
 
 (defn convert-number-part
-  [part]
-  (cond (< part 20) (convert-teens part)))
+  [part m]
+  (cond (< part 20) (convert-teens part)
+        (< part 100) (str (convert-tens-digit (/ part 10)) "-" (convert-single-digit (mod part 10)))
+        :else (str (convert-single-digit (/ part 100)) " " (get-place-descriptor  m))))
+
+(defn is-not-single-digit
+  "make sure that on the first time through a single digit is still processed"
+  [k m]
+  (not (and (= (count (str k)) 1) (= m 100))))
+
+(defn render-text
+  [s]
+  (str (first s) " and " (reduce str (interpose " and " (rest s)))))
 
 (defn convert-number
   ([number]
@@ -39,12 +55,9 @@
           s (cons decimal [])]
     (convert-number number 100 (mod (int number) 100) s)))
   ([number m k s]
-    (println number)
-    (println m)
-    (println k)
-    (println s)
     (if (and (< (+ 1 (count (str k))) (count (str m)) )
-             (not (and (= (count (str k)) 1) (= m 100))))
-      (str (first s) (reduce str " and " (rest s)))
-      (recur number (* m 10) (mod (int number) (* m 10)) (cons (convert-number-part k) s)))))
+             (is-not-single-digit k m))
+      (render-text s)
+      (recur number (* m 10) (mod (int number) (* m 10)) (cons (convert-number-part k m) s)))))
+
 
--- test/zee3/alphaconverter_test.clj
+++ test/zee3/alphaconverter_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [zee3.alphaconverter :refer :all]))
 
+(deftest test-get-place-descriptor
+  (testing "place descriptor retrieval"
+    (is (= "hundred" (get-place-descriptor 1000)))))
+
 (deftest test-convert-decimal
   (testing "decimal conversion"
     (is (= "23/100" (convert-decimal 0.23)))))
@@ -32,7 +36,7 @@
           (is (= "twenty" (convert-tens-digit 2)))
           (is (= "thirty" (convert-tens-digit 3)))
           (is (= "forty" (convert-tens-digit 4)))
-          (is (= "fifthy" (convert-tens-digit 5)))
+          (is (= "fifty" (convert-tens-digit 5)))
           (is (= "sixty" (convert-tens-digit 6)))
           (is (= "seventy" (convert-tens-digit 7)))
           (is (= "eighty" (convert-tens-digit 8)))
@@ -65,9 +69,16 @@
   (testing "convert number to string representation"
     (is (= "ten and 0/100" (convert-number 10)))
     (is (= "one and 0/100" (convert-number 1)))
-    (is (= "eighteen and 25/100" (convert-number 18.25)))))
+    (is (= "eighteen and 25/100" (convert-number 18.25)))
+    (is (= "fifty-six and 98/100" (convert-number 56.98)))
+    (is (= "two hundred and thirty-four and 89/100" (convert-number 234.89)))))
 
 (deftest test-convert-number-part
   (testing "convert part of number"
-    (is (= "one"  (convert-number-part 1)))
-    (is (= "eleven"  (convert-number-part 11)))))
+    (is (= "one"  (convert-number-part 1 100)))
+    (is (= "eleven"  (convert-number-part 11 100)))))
+
+(deftest test-render-text
+  (testing "rendering text"
+    (is (= "ten and 0/100" (render-text ["ten" "0/100"])))
+    (is (= "two hundred and thirty-four and 89/100" (render-text ["two hundred" "thirty-four" "89/100"])))))
